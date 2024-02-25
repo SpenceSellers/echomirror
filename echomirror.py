168a4dfca1d160dca5101ced1f5e47ae27eb8dcc
Spence Sellers <spencesellers@gmail.com>
2024-02-24 22:53:03 -0600

    Don't forward transfer-encoding or content-encoding headers because we don't support them.

--- echomirror.py
+++ echomirror.py
@@ -7,24 +7,54 @@ import requests
 from urllib import parse
 
 
-
-
-
 @click.command()
-@click.option('--port', '-p', default=8080, show_default=True,
-              help='The port that echomirror will listen to')
-@click.option('--status-code', '-s', default=200, show_default=True,
-              help='The HTTP status code we will return')
-@click.option('--text', default=None, type=str,
-              help='Respond to requests with this text using the text/plain content-type')
-@click.option('--json', default=None, type=str,
-              help='Respond to requests with this JSON using the application/json content-type')
-@click.option('--expose/--localhost-only', default=False, show_default=True, is_flag=True,
-              help='Whether to expose this port to the outside network (i.e., host on 0.0.0.0), or only allow localhost '
-                   'connections.')
-@click.option('--proxy', type=str,
-              help='Send intercepted requests to the specified URL and return the responses back to the client. This allows you to \'spy\' on requests made to a remote server.')
-def main(port, status_code: int, text: Optional[str], json: Optional[str], expose: bool, proxy: Optional[str]) -> None:
+@click.option(
+    "--port",
+    "-p",
+    default=8080,
+    show_default=True,
+    help="The port that echomirror will listen to",
+)
+@click.option(
+    "--status-code",
+    "-s",
+    default=200,
+    show_default=True,
+    help="The HTTP status code we will return",
+)
+@click.option(
+    "--text",
+    default=None,
+    type=str,
+    help="Respond to requests with this text using the text/plain content-type",
+)
+@click.option(
+    "--json",
+    default=None,
+    type=str,
+    help="Respond to requests with this JSON using the application/json content-type",
+)
+@click.option(
+    "--expose/--localhost-only",
+    default=False,
+    show_default=True,
+    is_flag=True,
+    help="Whether to expose this port to the outside network (i.e., host on 0.0.0.0), or only allow localhost "
+    "connections.",
+)
+@click.option(
+    "--proxy",
+    type=str,
+    help="Send intercepted requests to the specified URL and return the responses back to the client. This allows you to 'spy' on requests made to a remote server.",
+)
+def main(
+    port,
+    status_code: int,
+    text: Optional[str],
+    json: Optional[str],
+    expose: bool,
+    proxy: Optional[str],
+) -> None:
     class MyServer(BaseHTTPRequestHandler):
         method: str = None
         proxy_response: Optional[ProxyResponseData] = None
@@ -33,18 +63,29 @@ def main(port, status_code: int, text: Optional[str], json: Optional[str], expos
             if proxy:
                 url = concat_urls(proxy, self.path)
                 proxy_request_headers = self.build_proxy_request_headers()
-                response = requests.request(method=self.method, url=url, headers=proxy_request_headers)
-                self.proxy_response = ProxyResponseData(url, response.status_code, response.content, cast(Dict[str, str], response.headers))
+                response = requests.request(
+                    method=self.method, url=url, headers=proxy_request_headers
+                )
+                self.proxy_response = ProxyResponseData(
+                    url,
+                    response.status_code,
+                    response.content,
+                    cast(Dict[str, str], response.headers),
+                )
 
                 self.send_response(response.status_code)
                 for proxy_response_header, value in response.headers.items():
-                    if proxy_response_header.lower() == 'transfer-encoding' and value.lower() == 'chunked':
-                        continue  # We don't support chunked transfer on our response, we're going to send it all at once.
+                    if proxy_response_header.lower() == "transfer-encoding":
+                        # We don't support chunked or any other special transfer modes on our response,
+                        # we're going to send it all at once.
+                        continue
+                    if proxy_response_header.lower() == "content-encoding":
+                        # We don't support gzip or any other encoding, it's going to be sent as plaintext
+                        continue
                     self.send_header(proxy_response_header, value)
 
                 self.end_headers()
 
-                # TODO handle chunked encoding here.
                 self.wfile.write(response.content)
 
             else:
@@ -55,7 +96,7 @@ def main(port, status_code: int, text: Optional[str], json: Optional[str], expos
                     self.send_header("Content-type", "application/json")
 
                 self.end_headers()
-                self.wfile.write(bytes(text or json or '', "utf-8"))
+                self.wfile.write(bytes(text or json or "", "utf-8"))
             self.log_request_and_response()
 
         def build_proxy_request_headers(self):
@@ -65,37 +106,39 @@ def main(port, status_code: int, text: Optional[str], json: Optional[str], expos
                 if len(request_header_values) > 1:
                     click.secho(
                         f"echomirror only supports one value per header key, but {request_header_values} has {len(request_header_values)} values. Sending only the first one.",
-                        fg='yellow', err=True)
+                        fg="yellow",
+                        err=True,
+                    )
                 proxy_request_headers[proxy_request_header] = request_header_values[0]
             proxy_request_headers["Host"] = parse.urlparse(proxy).hostname
             return proxy_request_headers
 
         def do_GET(self):
-            self.method = 'GET'
+            self.method = "GET"
             self.handle_request()
 
         def do_POST(self):
-            self.method = 'POST'
+            self.method = "POST"
             self.handle_request()
 
         def do_PUT(self):
-            self.method = 'PUT'
+            self.method = "PUT"
             self.handle_request()
 
         def do_DELETE(self):
-            self.method = 'DELETE'
+            self.method = "DELETE"
             self.handle_request()
 
         def do_HEAD(self):
-            self.method = 'HEAD'
+            self.method = "HEAD"
             self.send_response(200)
-            self.send_header('Content-type', 'text/html')
+            self.send_header("Content-type", "text/html")
             self.end_headers()
 
         def do_OPTIONS(self):
-            self.method = 'OPTIONS'
+            self.method = "OPTIONS"
             self.send_response(204)
-            self.send_header('Allow', 'GET, POST, PUT, DELETE, OPTIONS')
+            self.send_header("Allow", "GET, POST, PUT, DELETE, OPTIONS")
             self.end_headers()
 
         def log_request(self, format, *args):
@@ -104,20 +147,23 @@ def main(port, status_code: int, text: Optional[str], json: Optional[str], expos
 
         def log_request_and_response(self):
             click.echo("")
-            click.secho(f"--> {self.method} {self.path}", fg='green')
+            click.secho(f"--> {self.method} {self.path}", fg="green")
             for header_name in self.headers:
                 for header_value in self.headers.get_all(header_name):
                     click.echo(f"    {header_name}: {header_value}")
-            content_len = self.headers.get('Content-Length');
+            content_len = self.headers.get("Content-Length")
             if content_len:
-                click.echo(self.rfile.read(int(content_len)).decode('utf-8'))
+                click.echo(self.rfile.read(int(content_len)).decode("utf-8"))
 
             if self.proxy_response:
-                click.secho(f"<-- {self.proxy_response.status} response proxied from {self.proxy_response.url}", fg='green')
+                click.secho(
+                    f"<-- {self.proxy_response.status} response proxied from {self.proxy_response.url}",
+                    fg="green",
+                )
                 for header_name, header_value in self.proxy_response.headers.items():
                     click.echo(f"    {header_name}: {header_value}")
 
-                click.secho(self.proxy_response.response.decode('utf-8'))
+                click.secho(self.proxy_response.response.decode("utf-8"))
 
         def headers_as_dict(self) -> Dict[str, str]:
             d = {}
@@ -125,17 +171,16 @@ def main(port, status_code: int, text: Optional[str], json: Optional[str], expos
                 d[header] = self.headers[header]
             return d
 
-
     if text and json:
         click.echo("--text and --json cannot be used at the same time.", err=True)
         return
 
-    host = 'locahost' if expose else '0.0.0.0'
+    host = "locahost" if expose else "0.0.0.0"
 
     server = HTTPServer((host, port), MyServer)
 
     try:
-        click.secho(f"Serving on http://{host}:{port}", fg='cyan')
+        click.secho(f"Serving on http://{host}:{port}", fg="cyan")
         server.serve_forever()
     except KeyboardInterrupt:
         pass
@@ -153,15 +198,16 @@ def concat_urls(a: str, b: str) -> str:
     """
     concat_urls('http://example.com/foo', '/bar') becomes 'http://example.com/foo/bar'
 
-    This is not the same as urljoin's behavior, which will treat the 2nd argument as an absolute path"""
-    if not a.endswith('/'):
-        a += '/'
+    This is not the same as urljoin's behavior, which will treat the 2nd argument as an absolute path
+    """
+    if not a.endswith("/"):
+        a += "/"
 
-    if b.startswith('/'):
-        b = b.rstrip('/')
+    if b.startswith("/"):
+        b = b.rstrip("/")
 
     return a + b
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
